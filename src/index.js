d8bd3b4d7e43fa880a276979f3497156a59f14e2
sdawn29 <snehasishdawn29@gmail.com>
2020-11-17 11:56:07 +0530

    Added jenkinsfile

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ var http = require("http");
 
 //create a server object:
 http
-  .createServer(function(req, res) {
+  .createServer(function (req, res) {
     res.write("Hello Earth!"); //write a response to the client
     res.end(); //end the response
   })
